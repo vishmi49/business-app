18ed83f76ec47c014b2373f257e087b725451884
Vishmi Perera <vishmi@vishmis-mbp.home>
2026-04-01 15:22:47 +0200

    create database connection

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const connectDB = require('./src/config/db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.use(express.json());
 
